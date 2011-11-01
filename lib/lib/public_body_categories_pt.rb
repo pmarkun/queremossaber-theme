dd7f7db938a1b2d1d808d38e397d168e5a994650
Pedro Markun <pedro@esfera.mobi>
2011-11-01 16:12:16 -0200

    first version of pt tags

---
+++ lib/lib/public_body_categories_pt.rb
@@ -0,0 +1,21 @@
+PublicBodyCategories.add(:pt, [
+    "Esfera",
+        [ "federal", "Federal", "Orgãos da esfera federal" ],
+        [ "estadual", "Estadual", "Orgãos da esfera estadual" ],
+        [ "municipal", "Municipal", "Orgãos da esfera municipal" ],
+    "Poder",
+        [ "executivo", "Executivo", "Orgãos do poder executivo"],
+        [ "legislativo", "Legislativo", "Orgãos do poder legislativo"],
+        [ "judiciario", "Judiciário", "Orgãos do poder judiciário"],
+    "Área de Atuação",
+        [ "saude", "Saúde", "Orgãos que trabalham com saúde" ],
+        [ "educacao", "Educação", "Orgãos que trabalham com educação" ],
+        [ "cultura", "Cultura", "Orgãos que trabalham com cultura" ],
+    "Tipos comuns",
+        [ "ministerio", "Ministérios", "Ministérios"],
+        [ "governo", "Governos Estaduais", "Governos Estaduais"],
+        [ "prefeitura", "Prefeituras", "Prefeituras"],
+    "Outros",
+        [ "outros", "Outros", "Outros"]
+])
+
